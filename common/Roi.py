7b3e305057b47e6af62bc3d81f678cc742505327
Jérôme Colin <jerome.colin@cesbio.cnes.fr>
2020-04-24 14:03:10 +0200

    Roistats with cloud and edge mask, valid pixel count and percentage

--- common/Roi.py
+++ common/Roi.py
@@ -70,23 +70,44 @@ class Roi_collection:
         for i in range(len(self.coord_arr)):
             # Get an ROI object
             roi_n = Roi(self.coord_arr[i], self.extent, logger)
+            mask = roi_n.get_mask(product, logger)
 
             # For each band in product, extract a subset according to ROI and return stats
             for band in bands:
                 # samples, minmax, avg, variance, skewness, kurtosis
-                stats = self.compute_stats_oneband(roi_n, product, band)
+
+                band_size = mask.size
+
+                stats = self.compute_stats_oneband(roi_n, product, band, mask=mask)
                 list_stats.append(stats)
                 if stdout:
-                    print("%s, %s, %s, %i, %6.4f, %6.4f, %6.4f, %6.4f, %6.4f, %6.4f" %
-                          (product.name, roi_n.id, band[:-1], stats[0], stats[1][0], stats[1][1], stats[2], stats[3],
-                           stats[4],
-                           stats[5]))
+                    if stats is not None:
+                        print("%s, %s, %s, %i, %i, %6.1f%%, %6.4f, %6.4f, %6.4f, %6.4f, %6.4f, %6.4f" %
+                              (product.name, roi_n.id, band[:-1], band_size, stats[0], stats[0]/band_size*100, stats[1][0], stats[1][1], stats[2], stats[3],
+                               stats[4],
+                               stats[5]))
+                    else:
+                        print("%s, %s, %s, no valid pixel in ROI (fully cloudy or out of edge)" % (product.name, roi_n.id, band[:-1]))
 
         return list_stats
 
-    def compute_stats_oneband(self, roi, product, band):
+    def compute_stats_oneband(self, roi, product, band, mask=None):
+        """
+
+        :param roi: Roi object
+        :param product: Product object
+        :param band: a string that helps identify a file
+        :return:
+        """
         subset = roi.cut_band(product, band, self.logger)
-        return stats.describe(subset, axis=None)
+        if mask is not None:
+            search = np.where(mask == 1)
+            valid_pixels = subset[search]
+
+        try:
+            return stats.describe(valid_pixels, axis=None)
+        except ValueError:
+            return None
 
 
 class Roi:
@@ -114,7 +135,7 @@ class Roi:
         """
         Returns a numpy array of a given band of a given product cut to fit the ROI
         :param product: a Product instance
-        :param band: a string that helps identify a file in the zipped Product
+        :param band: a string that helps identify a file
         :param logger:
         :return: a numpy array
         """
@@ -133,6 +154,21 @@ class Roi:
         return product._get_zipped_band_subset_asarray(
             product.find_band(band), logger, ulx=self.ulx,
             uly=self.uly, lrx=self.lrx, lry=self.lry)
+
+    def get_mask(self, product, logger):
+        # Get once the clm if any
+        clm = self.cut_mask(product, product.clm_name, logger)
+        edg = self.cut_mask(product, product.edg_name, logger)
+        mask = clm + edg
+
+        dummy = np.zeros_like(clm) + 1
+        search = np.where(mask != 0)
+        dummy[search] = 0
+
+        logger.debug("mask_NaNsum=%i, dummy_NaNsum=%i" % (np.nansum(mask), np.nansum(dummy)))
+
+        return dummy
+
 
     def get_stacked_asarray(self, product, logger):
         pass
