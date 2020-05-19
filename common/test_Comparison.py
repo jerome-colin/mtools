30876c92df9a239d6fd70f4f734c88fc1426dd35
Jérôme Colin <jerome.colin@cesbio.cnes.fr>
2020-05-19 11:12:26 +0200

    Minor bugfix in test Comparison

--- common/test_Comparison.py
+++ common/test_Comparison.py
@@ -26,5 +26,5 @@ def test_Comparison_basic():
 
     compare = Comparison.Comparison(acix_vermote_collection, acix_maja_collection, logger)
     assert sorted(compare.matching_products, key = lambda x: x[1])[0][0] == "20171005"
-    assert sorted(compare.matching_products, key = lambda x: x[1])[0][1] == "/home/colin/code/mtools/test_data/vermote_carpentras/refsrs2-L1C_T31TFJ_A003037_20171005T104550-Carpentras.hdf"
-    assert sorted(compare.matching_products, key = lambda x: x[1])[0][2] == "/home/colin/code/mtools/test_data/acix_carpentras/SENTINEL2B_20171005-104550-197_L2A_T31TFJ_C_V1-0"
+    assert sorted(compare.matching_products, key = lambda x: x[1])[0][1] == TEST_DATA_PATH + "vermote_carpentras/refsrs2-L1C_T31TFJ_A003037_20171005T104550-Carpentras.hdf"
+    assert sorted(compare.matching_products, key = lambda x: x[1])[0][2] == TEST_DATA_PATH + "acix_carpentras/SENTINEL2B_20171005-104550-197_L2A_T31TFJ_C_V1-0"
