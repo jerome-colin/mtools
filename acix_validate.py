3ea06cd4f894210011568e4bc3bcd1e040e999b2
Jérôme Colin <jerome.colin@cesbio.cnes.fr>
2020-05-26 17:54:09 +0200

    Per band, with samples

--- acix_validate.py
+++ acix_validate.py
@@ -14,6 +14,7 @@ __version__ = "1.0.2"
 import sys
 import argparse
 import numpy as np
+from matplotlib import pylab as pl
 import common.utilities as utl
 import common.Product as prd
 import common.Collection as clc
@@ -24,6 +25,8 @@ def main():
     # Argument parser
     parser = argparse.ArgumentParser()
     parser.add_argument("list", help="List of paths of collection")
+    parser.add_argument("--band", help="Specific band in acix band definition", type=int, required=True)
+    parser.add_argument("--samples", help="Reflectance sampling, defaults to 100 (ie. 0.01)", type=int, default=100)
     parser.add_argument("-s", "--save", help="Write location results as npy instead of stacking in memory", action="store_true", default=False)
     parser.add_argument("-v", "--verbose", help="Set verbosity to DEBUG level", action="store_true", default=False)
     args = parser.parse_args()
@@ -44,25 +47,24 @@ def main():
         ["band11", "SRE_B11.", "R2", []],
         ["band12", "SRE_B12.", "R2", []])
 
+    band_id = args.band
+    if (band_id < 0 or band_id > len(bdef_acix)):
+        logger.error("Band ID out of range with value %i" % band_id)
+        sys.exit(3)
+
+    samples = args.samples
+    step = 1 / samples
+
+    final_stats = ([])
+    for s in range(samples):
+        final_stats.append([])
+
     f = open(args.list, 'r')
     paths_list = f.read().splitlines()
 
     for p in paths_list:
         paths = p.split(',')
-        location_name = paths[0].split('/')[-1]
-
-        # Container for local statistics
-        local_stats = (
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [],
-            [])
+        #location_name = paths[0].split('/')[-1]
 
         acix_vermote_collection = clc.Collection(paths[0], logger)
         acix_maja_collection = clc.Collection(paths[1], logger)
@@ -73,49 +75,92 @@ def main():
             p_ref = prd.Product_hdf_acix(match[1], logger)
             p_maja = prd.Product_dir_maja(match[2], logger)
 
-            for b in range(len(bdef_acix)):
-                b_ref = p_ref.get_band(p_ref.find_band(bdef_acix[b][0]), scalef=p_ref.sre_scalef)
-                b_maja = p_maja.get_band(p_maja.find_band(bdef_acix[b][1]), scalef=p_maja.sre_scalef)
-                clm = p_maja.get_band(p_maja.find_band("CLM_" + bdef_acix[b][2]))
-                edg = p_maja.get_band(p_maja.find_band("EDG_" + bdef_acix[b][2]))
-                mask, ratio = p_maja.get_mask(clm, edg, stats=True)
-                del clm
-                del edg
+            b_ref = p_ref.get_band(p_ref.find_band(bdef_acix[band_id][0]), scalef=p_ref.sre_scalef)
+            b_maja = p_maja.get_band(p_maja.find_band(bdef_acix[band_id][1]), scalef=p_maja.sre_scalef)
+            clm = p_maja.get_band(p_maja.find_band("CLM_" + bdef_acix[band_id][2]))
+            edg = p_maja.get_band(p_maja.find_band("EDG_" + bdef_acix[band_id][2]))
+            mask, ratio = p_maja.get_mask(clm, edg, stats=True)
+            del clm
+            del edg
 
-                b_ref_valid = utl.is_valid(b_ref, mask)
-                del b_ref
-                b_maja_valid = utl.is_valid(b_maja, mask)
-                del b_maja
-                del mask
+            b_ref_valid = utl.is_valid(b_ref, mask)
+            del b_ref
+            b_maja_valid = utl.is_valid(b_maja, mask)
+            del b_maja
+            del mask
 
-                if len(b_ref_valid) == len(b_maja_valid):
-                    local_stats[b].extend(b_ref_valid - b_maja_valid)
-                    #local_stats[b][1].extend(b_maja_valid)
-                    if not args.save:
-                        bdef_acix[b][3].extend(b_ref_valid - b_maja_valid)
-                        #bdef_acix[b][4].extend(b_maja_valid)
-                    else:
-                        np.save(location_name + '_' + bdef_acix[b][0] + '.npy', np.stack((b_ref_valid, b_maja_valid)))
+            if len(b_ref_valid) == len(b_maja_valid):
 
-                else:
-                    logger.error("Length unmatch between %s and %s" % (bdef_acix[b][0], bdef_acix[b][1]))
+                for s in range(samples):
+                    for i in range(len(b_maja_valid)):
+                        if b_maja_valid[i] >= (s * step - step / 2) and b_maja_valid[i] < (s * step + step / 2):
+                            final_stats[s].extend([b_ref_valid[i] - b_maja_valid[i]])
 
-        for l in range(len(local_stats)):
-            rmse = utl.rmse_d(np.asarray(local_stats[l]))
-            acix_A = utl.accuracy(np.asarray(local_stats[l]))
-            acix_P = utl.precision(np.asarray(local_stats[l]))
-            acix_U = utl.uncertainty(np.asarray(local_stats[l]))
-            logger.info("RESULT, %s, %s, %8.6f, %8.6f, %8.6f, %8.6f" % (location_name, bdef_acix[l][0], rmse, acix_A, acix_P, acix_U))
+            else:
+                logger.error("Length unmatch between %s and %s" % (bdef_acix[band_id][0], bdef_acix[band_id][1]))
 
-        del local_stats
+    x_sr   = np.arange(0, 1, step) - (step / 2)
+    spec   = 0.005 + 0.05 * x_sr
+    n_sr   = np.zeros(samples)
+    acix_a = np.zeros(samples)
+    acix_p = np.zeros(samples)
+    acix_u = np.zeros(samples)
 
-    if not args.save:
-        for b in range(len(bdef_acix)):
-            rmse = utl.rmse_d(np.asarray(bdef_acix[b][3]))
-            acix_A = utl.accuracy(np.asarray(np.asarray(bdef_acix[b][3])))
-            acix_P = utl.precision(np.asarray(np.asarray(bdef_acix[b][3])))
-            acix_U = utl.uncertainty(np.asarray(np.asarray(bdef_acix[b][3])))
-            logger.info("RESULT, Stacked, %s, %8.6f, %8.6f, %8.6f, %8.6f" % (bdef_acix[b][0], rmse, acix_A, acix_P, acix_U))
+    full_stats = ([])
+
+    for s in range(samples):
+        n_sr[s] = len(final_stats[s])
+        if n_sr[s] == 0:
+            acix_a[s] = np.nan
+            acix_p[s] = np.nan
+            acix_u[s] = np.nan
+        else:
+            acix_a[s] = np.abs(utl.accuracy(np.asarray(final_stats[s])))
+            acix_p[s] = np.abs(utl.precision(np.asarray(final_stats[s])))
+            acix_u[s] = np.abs(utl.uncertainty(np.asarray(final_stats[s])))
+        full_stats.extend(final_stats[s])
+
+        logger.debug("RESULT, %s, %s, %8.3f, %8.3f, %i, %8.6f, %8.6f, %8.6f" %
+                    ("STACKED",
+                     bdef_acix[band_id][0],
+                     s * step,
+                     s * step + step,
+                     n_sr[s],
+                     acix_a[s],
+                     acix_p[s],
+                     acix_u[s]))
+
+    full_acix_a = np.abs(utl.accuracy(np.asarray(full_stats)))
+    full_acix_p = np.abs(utl.precision(np.asarray(full_stats)))
+    full_acix_u = np.abs(utl.uncertainty(np.asarray(full_stats)))
+    full_n_sr = len(full_stats)
+
+    logger.info("RESULT, %s, %s, %i, %8.6f, %8.6f, %8.6f" %
+                 ("STACKED",
+                  bdef_acix[band_id][0],
+                  full_n_sr,
+                  full_acix_a,
+                  full_acix_p,
+                  full_acix_u))
+
+    fig, ax1 = pl.subplots(figsize=(8, 8), dpi=300)
+
+    pl.title('%s \n A=%8.6f, P=%8.6f, U=%8.6f' % (bdef_acix[band_id][0], full_acix_a, full_acix_p, full_acix_u))
+
+    pl.xlim(0,0.6)
+    ax1.set_xlabel("Surface reflectance (-)")
+    ax1.set_ylim(0, 0.03)
+    ax1.plot(x_sr, acix_a, 'r-2', label='accuracy')
+    ax1.plot(x_sr, acix_p, 'g-2', label='precision')
+    ax1.plot(x_sr, acix_u, 'b-2', label='uncertainty')
+    ax1.plot(x_sr, spec, 'm', label='suggested specs')
+    pl.legend()
+
+    ax2 = ax1.twinx()
+    ax2.bar(x_sr, n_sr, fill=False, color='w', edgecolor='c', width=0.01, label="nb of points")
+
+    pl.legend()
+    pl.savefig("acix_stacked_" + bdef_acix[band_id][0] + ".png")
 
     sys.exit(0)
 
